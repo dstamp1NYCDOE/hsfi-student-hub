8434c059735d4870d031696e22bccc8c6aacfae1
Derek Stampone <dstampone@schools.nyc.gov>
2025-03-11 12:04:29 -0400

    Add page for College Board Day

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ function onSubmit(event){
         if (sheet_title == 'FirstDayRoomAssignment'){
             returnFirstDayOfSchoolHTML(StudentInfo)
         }
+        if (sheet_title == 'PSATRoomAssignment'){
+            returnPSATRoomAssignmentHTML(StudentInfo)
+        }        
         
         for (span of btn_toggles){
             span.classList.toggle('visually-hidden')
@@ -50,7 +53,7 @@ function onSubmit(event){
     <div class="card" style="background-color:red">
         <div class="card-body">
             <h5 class="card-title">Student Not Found</h5>
-            <p class="card-text">A student with the OSIS ${StudentID} is not available in the data.  Please check your StudentID number and try again.</p>
+            <p class="card-text">A student with the OSIS ${StudentID} is not available in the data.  Please check your StudentID number and try again. Note: This error can happen when many students are accessing the site simultaneously.</p>
         </div>
     </div>
     `    
@@ -98,6 +101,51 @@ function returnFirstDayOfSchoolHTML(StudentInfo){
         <ul class="list-group list-group-flush">
             <li class="list-group-item">${FirstName} ${LastName}</li>
             <li class="list-group-item"><strong>Room ${Room}</strong></li>
+        </ul>
+    </div>
+    `
+
+}
+
+
+
+function returnPSATRoomAssignmentHTML(StudentInfo){
+    var StudentID = StudentInfo.StudentID
+    var LastName = StudentInfo.LastName
+    var FirstName = StudentInfo.FirstName
+    var Room = StudentInfo.Room
+    var AssignedActivity = StudentInfo.AssignedActivity
+    var Assignment = StudentInfo.Assignment
+    var ReportDate = StudentInfo.ReportDate
+    var ReportTime = StudentInfo.ReportTime
+
+    background_color = "white";
+    switch (AssignedActivity) {
+    case 'PSAT':
+        background_color = "pink";
+        break;
+    case 'SAT':
+        background_color = "yellow";
+        break;
+    case 'CTE_Studio':
+        background_color = "green";
+        break;
+    }
+
+
+    var response_div = document.getElementById("response")
+
+    response_div.innerHTML = `
+    <div class="card" style="background-color:${background_color}">
+        <div class="card-body">
+            <h5 class="card-title">P/SAT Day Room Assignment</h5>
+            <p class="card-text">This is your assigned room location for the SAT or PSAT day - can be different - on ${ReportDate}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${FirstName} ${LastName}</li>
+            <li class="list-group-item">Assignment:${AssignedActivity} ${Assignment}</li>
+            <li class="list-group-item"><strong>Room ${Room}</strong></li>
+            <li class="list-group-item"><strong>Report Time ${ReportTime}</strong></li>
         </ul>
     </div>
     `
